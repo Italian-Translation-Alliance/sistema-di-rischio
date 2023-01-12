73f86fc389274ca80a5bce7c9ef9c72bfc781251
zeruhur <51272598+zeruhur@users.noreply.github.com>
2023-01-12 10:20:52 +0100

    site update

--- docs/docs/_build.sh
+++
@@ -1,8 +0,0 @@
-pandoc -f gfm sistema-di-rischio.md -o SistemaDiRischio.pdf -V geometry:margin=2.8cm -V page_size:A4
-pandoc -f gfm sistema-di-rischio.md -o SistemaDiRischio.odt
-generate-md --layout jasonm23-foghorn  --input ./ --output ./docs
-cd docs
-mv sistema-di-rischio.html index.html
-cd ..
-rm SistemaDiRischio.odt
-rm SistemaDiRischio.pdf
